f10d054c1e4f8fbb4df0aafd69166d3a96aa611f
or shwartz <or shwartz>
2017-04-28 13:08:47 +0300

    the second one

--- alert_system.py
+++ alert_system.py
@@ -21,7 +21,9 @@ class ClientIntel:
         self.clients_dict[serial_num] = client_intel
 
     def get_client(self, serial_num):
-        return self.clients_dict[serial_num]
+        if serial_num in self.clients_dict:
+            return self.clients_dict[serial_num]
+        print("no client {}".format(serial_num))
 
 
 class ErrorToMsg:
@@ -29,7 +31,6 @@ class ErrorToMsg:
         self.error_dict = {}
 
     def add_error_msg(self, error_id, msg):
-        print(error_id)
         self.error_dict[error_id] = msg
 
     def get_error_msg(self, error_id):
@@ -45,17 +46,23 @@ class CheckAlert:
 
     def get_alert(self):
         response = requests.get(url=self.host)
-        return json.loads(response.text)["acId"]
+        j = json.loads(response.text)
+        print(j)
+        if "acId" in j:
+            return j["acId"]
 
 
-def alert_job(alerts_, client_intel, error_to_msg):
+def alert_job(alert_list_arr, alerts_, client_intel, error_to_msg):
     alert_list = alerts_.get_alert()
-    if len(alert_list) == 0:
+    if not alert_list:
+        print("No list")
         return
 
-    sent = False
     for client_id in alert_list:
         client = client_intel.get_client(client_id)
+        if not alert_list[client_id]["errorCode"]:
+            print("no error message for {}".format(alert_list[client_id]["errorCode"]))
+            continue
         error_msg = error_to_msg.get_error_msg(str(alert_list[client_id]["errorCode"]))
         client_sms = SendSms(client['number'])
 
@@ -66,24 +73,26 @@ def alert_job(alerts_, client_intel, error_to_msg):
                                                                  SUPPORT_CALL_YOU_MSG,
                                                                  SUPPORT_LINK,
                                                                  END_OF_MSG)
-        client_sms.send_sms(client_sms_msg)
-        sent = True
-    return sent
+        if alert_list[client_id]["errorCode"] not in alert_list_arr:
+            client_sms.send_sms(client_sms_msg)
+            alert_list_arr.append(alert_list[client_id]["errorCode"])
+            print("sent message")
 
 
 if __name__ == '__main__':
     # Fill DB.
     client_db = ClientIntel()
-    client_db.add_client({'name': 'אור שורץ', 'number': '+972528727102'}, '33333333')
-    client_db.add_client({'name': 'אור שורץ', 'number': '+972528727102'}, '22222222')
-    client_db.add_client({'name': 'גיא עשת', 'number': '+972528727102'}, '11111111')
+    client_db.add_client({'name': 'גיא עשת', 'number': '+972546626752'}, '3763200171')
+    client_db.add_client({'name': 'גיא עשת', 'number': '+972546626752'}, '2')
+    client_db.add_client({'name': 'גיא עשת', 'number': '+972546626752'}, '3')
     error_db = ErrorToMsg()
-    error_db.add_error_msg('2.0', "{}:\n{}".format(FILTER_ERROR_MSG, CHANGE_FILTER_VIDEO))
-    error_db.add_error_msg('8.0', "{}.\n{}: {}".format("התקלה מצריכה קריאה לטכנאי", ORDER_TECH_MSG, CALENDER_LINK))
+    error_db.add_error_msg('1.0', "{}.\n{}: {}".format("התקלה מצריכה קריאה לטכנאי", ORDER_TECH_MSG, CALENDER_LINK))
+    error_db.add_error_msg('1.0', "{}:\n{}".format(FILTER_ERROR_MSG, CHANGE_FILTER_VIDEO))
+    error_db.add_error_msg('3.0', "{}.\n{}: {}".format("התקלה מצריכה קריאה לטכנאי", ORDER_TECH_MSG, CALENDER_LINK))
+    error_db.add_error_msg('4.0', "{}.\n{}: {}".format("התקלה מצריכה קריאה לטכנאי", ORDER_TECH_MSG, CALENDER_LINK))
     # Set alert server connection.
     alerts = CheckAlert('http://104.199.88.197/get_errors/')
-    alert_job(alerts, client_db, error_db)
+    error_list = []
     while True:
-        time.sleep(60)
-        if alert_job(alerts, client_db, error_db):
-            break
+        time.sleep(1)
+        alert_job(error_list, alerts, client_db, error_db)
